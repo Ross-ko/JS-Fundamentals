f7f74963ebd0804c52ece1544f2b823672f347b1
Rosen Ignatov <rosen.ignatoff@gmail.com>
2023-10-15 19:51:41 +0300

    exercise

---
+++ Functions/Math_Power.js
@@ -0,0 +1,15 @@
+function mathPower(num, power) {
+
+    console.log(num ** power);
+
+    let result = 1;
+
+    for (i = 0; i < power; i++ ) {
+        result *= num;
+    }
+
+    console.log(result);
+
+}
+
+mathPower(2, 8);
